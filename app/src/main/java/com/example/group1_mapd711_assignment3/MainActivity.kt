47cec27f7150c138448caaabd1bfb78224b79ab0
Emre Deniz <emre.deniz@outlook.com>
2023-11-02 12:39:30 -0400

    Changes on activity files

--- app/src/main/java/com/example/group1_mapd711_assignment3/MainActivity.kt
+++ app/src/main/java/com/example/group1_mapd711_assignment3/MainActivity.kt
@@ -1,3 +1,11 @@
+/**
+ * @Group 1
+ * @author Muindo Gituku (301372521)
+ * @author Emre Deniz (301371047)
+ * @date Nov 3, 2023
+ * @description: Android Assignment 3 - Pizza Locator App
+ */
+
 package com.example.group1_mapd711_assignment3
 
 import android.content.Intent
@@ -10,21 +18,25 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    val cities_saved_list = arrayOf("Toronto Downtown, ON", "Scarborough, ON", "Mississauga, ON", "North York, ON", "Oakville, ON", "Markham, ON", "Brampton, ON", "Ajax, ON",)
+    // List of cities
+    val cities_saved_list = arrayOf("Toronto Downtown, ON", "Scarborough, ON", "Mississauga, ON", "North York, ON", "Oakville, ON", "Markham, ON", "Brampton, ON", "Ajax, ON")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
+        // ListView of Cities
         binding.citiesListView.setOnItemClickListener { parent, view, position, id ->
             val selectedCity = cities_saved_list[position]
 
+            // Define intent
             val intent = Intent(this,MarkersMapActivity::class.java)
+
+            // Pass selected city to intent
             intent.putExtra("selected_city",selectedCity)
 
+            // Get longtitude and latitude values of selected city
             val geocoder = Geocoder(this)
             val results = geocoder.getFromLocationName(selectedCity, 1)
             if (results != null) {
@@ -32,14 +44,13 @@ class MainActivity : AppCompatActivity() {
                     val location = results[0]
 
                     val latitude = location.latitude
-                    intent.putExtra("latitude",latitude)
-
                     val longitude = location.longitude
+
+                    // Pass latitude and longitude to intent
+                    intent.putExtra("latitude",latitude)
                     intent.putExtra("longitude",longitude)
 
                     startActivity(intent)
-                } else {
-
                 }
             }
         }
--- app/src/main/java/com/example/group1_mapd711_assignment3/MarkersMapActivity.kt
+++ app/src/main/java/com/example/group1_mapd711_assignment3/MarkersMapActivity.kt
@@ -1,5 +1,12 @@
-package com.example.group1_mapd711_assignment3
+/**
+ * @Group 1
+ * @author Muindo Gituku (301372521)
+ * @author Emre Deniz (301371047)
+ * @date Nov 3, 2023
+ * @description: Android Assignment 3 - Pizza Locator App
+ */
 
+package com.example.group1_mapd711_assignment3
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -18,7 +25,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-
 // Data classes for API response
 data class PlaceResult(val results: List<Place>)
 data class Place(val name: String, val vicinity: String, val geometry: Geometry)
@@ -31,7 +37,7 @@ interface PlacesApiService {
     suspend fun getNearbyPlaces(
         @Query("location") location: String,
         @Query("radius") radius: Int,
-        @Query("type") type: String,
+        @Query("name") name: String,
         @Query("key") apiKey: String
     ): PlaceResult
 }
@@ -41,6 +47,7 @@ class MarkersMapActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMarkersMapBinding
 
+    // Define place longitude and latitude values
     private var townLatitude = 0.0
     private var townLongitude = 0.0
     private var userSelectedCity = ""
@@ -51,54 +58,45 @@ class MarkersMapActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(binding.root)
 
         val intent = intent
-        userSelectedCity = "Restaurants in "+intent.getStringExtra("selected_city")
+        userSelectedCity = "Pizza Restaurants in "+intent.getStringExtra("selected_city")
         townLatitude = intent.getDoubleExtra("latitude", 0.00)
         townLongitude = intent.getDoubleExtra("longitude", 0.00)
 
         binding.selectedCityText.text = userSelectedCity
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+        // Obtain the SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-
-    override  fun onMapReady(googleMap: GoogleMap) {
+    // Manipulate the map once available
+    override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker to selected town and move the camera
+        // Add a marker to selected town
         val selectedTownPin = LatLng(townLatitude, townLongitude)
         mMap.addMarker(MarkerOptions().position(selectedTownPin).title(userSelectedCity))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(selectedTownPin, 15f))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(selectedTownPin, 13f))
 
-        fetchNearbyRestaurants()
+        // Call fetch Nearby Pizza Restaurants function
+        fetchNearbyPizzaRestaurants()
     }
 
-    private  fun fetchNearbyRestaurants(){
-        // Coordinates of the location (e.g., latitude: 40.7128, longitude: -74.0060 for New York)
+    private fun fetchNearbyPizzaRestaurants(){
+
+        // Google Places API key
+        val apiKey = "AIzaSyDBQOf0tvMNiecFD8A5UQ-0JmHW8BL6JAA"
+
+        // Coordinates of the location
         val latitude = townLatitude
         val longitude = townLongitude
         val location = "$latitude,$longitude"
 
-        // Radius in meters (1 kilometer)
-        val radius = 1000
+        // Radius in meters (2 kilometer)
+        val radius = 2000
 
-        // Type of place you want to filter (e.g., "restaurant", "cafe")
-        val type = "restaurant"
-
-        // Your Google Places API key
-        val apiKey = "AIzaSyDBQOf0tvMNiecFD8A5UQ-0JmHW8BL6JAA"
+        // Name of places to filter
+        val name = "pizza"
 
         // Retrofit setup
         val retrofit = Retrofit.Builder()
@@ -110,29 +108,27 @@ class MarkersMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
         lifecycleScope.launch {
             try {
-                // Make API call to get nearby places
+                // Make API call to get nearby restaurants
                 val response = placesApiService.getNearbyPlaces(
-                    location, radius, type, apiKey
+                    location, radius, name, apiKey
                 )
                 val places = response.results
 
-                // Add markers for each nearby restaurant
+                // Add markers for each nearby restaurants
                 for (place in places) {
                     val latLng = LatLng(place.geometry.location.lat, place.geometry.location.lng)
                     val markerOptions = MarkerOptions()
                         .position(latLng)
                         .title(place.name)
                         .snippet(place.vicinity)
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))// Additional information, if desired
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
                     mMap.addMarker(markerOptions)
                 }
 
             } catch (e: Exception) {
                 e.printStackTrace()
-                // Handle error
             }
         }
 
     }
 }
-
